9c9b4eda554390cf15f70fa8e5b9242e9ad18d99
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-09 11:23:08 -0800

    contain starter code for the pages and the index.js file to import them easily

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserHistory, Link, Route, Routes } from "@remix-run/router";
+import { Home, SelectItem } from "./pages";
 
 const App = () => {
   return <div>Hello World!</div>;
---
+++ client/src/pages/SelectItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SelectItem = () => {
+  return <div>This is the Selected Item Page</div>;
+};
+
+export default SelectItem;
