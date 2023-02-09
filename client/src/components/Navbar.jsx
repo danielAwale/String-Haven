be2dcbff607a70730d326053bd8066e4fafd5a8c
Daniel Awale <danielmadhukarawale@gmail.com>
2023-02-09 11:53:56 -0800

    contain the navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,15 +2,12 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <nav class="flex items-center justify-between p-4 bg-gray-900 text-white">
+    <nav class="flex items-center justify-between p-4 text-white bg-orange-500">
       <div class="flex items-center">
         <img src="logo.svg" alt="Logo" class="h-8 w-8" />
-        <h1 class="ml-2 font-medium">My Website</h1>
+        <h1 class="ml-2 font-medium">String Haven</h1>
       </div>
       <div class="flex">
-        <a href="#" class="px-4 py-2 hover:bg-gray-800">
-          Home
-        </a>
         <a href="#" class="px-4 py-2 hover:bg-gray-800">
           About
         </a>
