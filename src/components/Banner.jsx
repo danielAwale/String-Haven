56220a3f8e2f2c2c08803004ac65b3f653782abb
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 14:14:03 -0700

    contain the opacity in the image

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-import Image from "../assets/guitar.jpeg";
+import Image from "../assets/guitar.jpg";
 
 const Banner = () => {
   return (
     <div>
-      <img src={Image} alt="" />
+      <img src={Image} alt="" className="opacity-60" />
     </div>
   );
 };
