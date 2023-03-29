4d46eedee94bdcc151417916818ddac213f2468b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 12:05:13 -0700

    start from scratch

--- client/src/pages/SelectItem.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const SelectItem = () => {
-  return <div>This is the Selected Item Page</div>;
-};
-
-export default SelectItem;
--- client/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-import Home from "./Home";
-import SelectItem from "./SelectItem";
-
-export { Home, SelectItem };
