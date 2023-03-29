0942ec582559c6e919a39c30d05d407ed8515431
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 16:16:11 -0700

    banner looks good so far

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,15 +4,18 @@ import Image from "../assets/guitar.jpg";
 
 const Banner = () => {
   return (
-    <div className="relative">
-      <div className="absolute ml-4">
-        <p className="text-white">Shop the world's finest guitars</p>
-        <p className="text-white">Made with 100% Koa</p>
+    <div className="relative w-full">
+      <div className="w-full h-full bg-black absolute opacity-50"></div>
+      <div className="absolute flex flex-col w-full h-full items-center justify-center">
+        <p className="text-white text-[25px] font-semibold italic">
+          Shop the world's finest guitars
+        </p>
+        <p className="text-white text-[45px]">Made with 100% Koa</p>
         <button className=" bg-orange-500 px-2 py-2 rounded-md">
           Shop now
         </button>
       </div>
-      <img src={Image} alt="" className="lg:height:200px" />
+      <img src={Image} alt="" />
     </div>
   );
 };
