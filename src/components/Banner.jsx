43bda06a0c3e1544fba6284d1d1783b3e2e24edd
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 15:37:55 -0700

    contain some progress

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -5,10 +5,14 @@ import Image from "../assets/guitar.jpg";
 const Banner = () => {
   return (
     <div className="relative">
-      <div className="absolute">
-        <button className="">Learn More</button>
+      <div className="absolute ml-4">
+        <p className="text-white">Shop the world's finest guitars</p>
+        <p className="text-white">Made with 100% Koa</p>
+        <button className=" bg-orange-500 px-2 py-2 rounded-md">
+          Shop now
+        </button>
       </div>
-      <img src={Image} alt="" className="opacity-70" />
+      <img src={Image} alt="" className="lg:height:200px" />
     </div>
   );
 };
