3b5a3a5bf015f94adeebe0f1f1573d108f6d4b64
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 12:23:22 -0700

    Contain starter code and clean up

---
+++ src/components/Guitar.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Guitar = () => {
+  return <div>Guitar</div>;
+};
+
+export default Guitar;
---
+++ src/components/GuitarItem.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const GuitarItem = () => {
+  return <div>GuitarItem</div>;
+};
+
+export default GuitarItem;
