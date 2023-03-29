11e1242099838c17a153288597ce9abb38eaff58
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 12:46:29 -0700

    work on header, mobile first

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div>
-      <div>String Haven</div>
+    <div className="flex justify-between py-4 px-4 items-center">
+      <div className="font-bold italic text-[30px] cursor-pointer">
+        String Haven
+      </div>
+      <div className="text-[35px] cursor-pointer">
+        <GiHamburgerMenu />
+      </div>
     </div>
   );
 };
