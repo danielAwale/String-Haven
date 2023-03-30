53f3e61447c9ad6258983fc7cd9070bb72d3f053
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-29 17:46:36 -0700

    start work on the policy component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
+import Policy from "./components/Policy";
 
 const App = () => {
   return (
-    <div>
+    <div className="h-[1000px]">
       <Header />
       <Banner />
+      <div className=" mt-4">
+        <Policy />
+      </div>
       <div className="flex justify-center w-full items-center mt-6">
         <p className="text-xl font-bold">Shop Our Latest Collection</p>
       </div>
--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -1,9 +1,24 @@
 import React from "react";
 
+import { FaShippingFast } from "react-icons/fa";
+import { ImPriceTags } from "react-icons/im";
+import { TbTruckReturn } from "react-icons/tb";
+
 const Policy = () => {
   return (
-    <div>
-      <div></div>
+    <div className="bg-slate-400 w-full flex justify-between px-2 py-2">
+      <div>
+        <FaShippingFast />
+        <p>Free Shipping</p>
+      </div>
+      <div>
+        <ImPriceTags />
+        <p>Best Price for the quality</p>
+      </div>
+      <div>
+        <TbTruckReturn />
+        <p>Free returns for 30 days</p>
+      </div>
     </div>
   );
 };
