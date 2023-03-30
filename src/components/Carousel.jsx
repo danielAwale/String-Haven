12a6906db3918296eaa38ace0dd90f57479cd47b
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-30 16:41:27 -0700

    slider functionality should work normally

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -8,16 +8,63 @@ import Img2 from "../assets/guitar3.jpeg";
 import Img3 from "../assets/guitar4.jpeg";
 
 const Carousel = () => {
-  const settings = {
+  var settings = {
     dots: true,
-    infinite: true,
+    infinite: false,
     speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   };
 
   return (
     <Slider {...settings}>
+      <div>
+        <img
+          className="w-full h-[700px] object-contain"
+          src={Img1}
+          alt="First slide"
+        />
+      </div>
+      <div>
+        <img
+          className="w-full h-[700px] object-contain"
+          src={Img2}
+          alt="Second slide"
+        />
+      </div>
+      <div>
+        <img
+          className="w-full h-[700px] object-contain"
+          src={Img3}
+          alt="Third slide"
+        />
+      </div>
       <div>
         <img
           className="w-full h-[700px] object-contain"
