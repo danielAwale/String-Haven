5bd0502d6eafbc5eef944086cb35caab135508b9
danielAwale <danielmadhukarawale@gmail.com>
2023-03-29 21:43:56 -0700

    fix the responsive flex issue on small screens

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,8 +4,8 @@ import { GiHamburgerMenu } from "react-icons/gi";
 const Header = () => {
   return (
     <div className="flex justify-between py-4 px-4 items-center">
-      <div className="font-bold italic text-[30px] cursor-pointer">
-        String Haven
+      <div className="font-thin italic text-[30px] cursor-pointer">
+       <span className="font-bold">String</span> Haven
       </div>
       <div className="text-[35px] cursor-pointer">
         <GiHamburgerMenu />
--- src/components/Policy.jsx
+++ src/components/Policy.jsx
@@ -6,16 +6,16 @@ import { TbTruckReturn } from "react-icons/tb";
 
 const Policy = () => {
   return (
-    <div className="bg-slate-400 w-full flex justify-between px-2 py-2">
-      <div>
+    <div className="bg-slate-300 w-full flex flex-col justify-between px-2 py-2">
+      <div className="flex justify-center items-center gap-2">
         <FaShippingFast />
         <p> Free Shipping</p>
       </div>
-      <div>
+      <div className="flex justify-center items-center gap-2">
         <ImPriceTags />
         <p>Best Price for the quality</p>
       </div>
-      <div>
+      <div className="flex justify-center items-center gap-2">
         <TbTruckReturn />
         <p>Free returns for 30 days</p>
       </div>
