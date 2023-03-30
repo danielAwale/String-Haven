32d5a959f0381fe3d1935fa81f4fb89524174a12
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-30 10:46:31 -0700

    start work ont he footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
+import { BsInstagram, BsFacebook, BsTwitter } from "react-icons/bs";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="absolute bottom-0 py-6 px-3">
+      <div>
+        <p>@2023 String Haven Inc. All Rights Reserved </p>
+      </div>
+      <div>
+        <p>Follow our socials</p>
+        <div>
+          <div>
+            <BsInstagram />
+          </div>
+          <div>
+            <BsFacebook />
+          </div>
+          <div>
+            <BsTwitter />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
