8955b565f834d048faba2cf1e5b0b3048ddbe4c2
danielAwale <danielmadhukarawale@gmail.com>
2023-03-30 22:24:43 -0700

    fix the slider issue wih the react multi carousel

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 
+import Img from '../assets/guitar2.jpeg'
+import Img2 from '../assets/guitar3.jpeg'
+import Img3 from '../assets/guitar4.jpeg'
+
 const responsive = {
   superLargeDesktop: {
     // the naming can be any, depends on you.
@@ -13,23 +17,31 @@ const responsive = {
     items: 3
   },
   tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2
+    breakpoint: { max: 1024, min: 350 },
+    items: 1
   },
   mobile: {
-    breakpoint: { max: 464, min: 0 }, 
+    breakpoint: { max: 350, min: 0 }, 
     items: 1
   }
 };
 
 const Slider = () => {
   return (
-    <div>
+    <div className='container w-full lg:ml-80'>
       <Carousel responsive={responsive}>
-      <div>Item 1</div>
-      <div>Item 2</div>
-      <div>Item 3</div>
-      <div>Item 4</div>
+      <div className='flex justify-center'>
+        <img src={Img} alt="" className='h-[800px] object-contain'/>
+      </div>
+      <div className='flex justify-center'>
+      <img src={Img2} alt="" className='h-[800px] object-contain' />
+      </div>
+      <div className='flex justify-center'>
+      <img src={Img3} alt="" className='h-[800px] object-contain' />
+      </div>
+      <div className='flex justify-center'>
+      <img src={Img} alt="" className='h-[800px] object-contain'/>
+      </div>
       </Carousel>;
     </div>
   )
