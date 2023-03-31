8b7133862d1cdebf8e4eaeb643dabf5cc2f68a6e
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-31 13:59:09 -0700

    reduce the height of the images and create a new component for the landing page

--- src/components/Guitar.jsx
+++ src/components/Guitar.jsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-const Guitar = ({image}) => {
-  return <div>
-     <div className='flex flex-col justify-center items-center'>
-        <img src={image} alt="" className='h-[800px] object-contain'/>
-          <p className="text-[20px] font-semibold">Model SH-3</p>
-          <p className="text-[18px] font-semibold">$1,350</p>
-          <button className="py-2 px-4 bg-amber-500 rounded-xl mt-2">More Info</button>
+const Guitar = ({ image }) => {
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center">
+        <img src={image} alt="" className="h-[600px] object-contain" />
+        <p className="text-[20px] font-semibold">Model SH-3</p>
+        <p className="text-[18px] font-semibold">$1,350</p>
+        <button className="py-2 px-4 bg-amber-500 rounded-xl mt-2">
+          More Info
+        </button>
       </div>
-  </div>;
+    </div>
+  );
 };
 
 export default Guitar;
