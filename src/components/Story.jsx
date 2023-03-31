e34c148785aed213c9e3786a316f4b022ebefde0
Daniel Awale <danielmadhukarawale@gmail.com>
2023-03-31 16:45:51 -0700

    contain a div with opacity

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -4,6 +4,7 @@ import Image from "../assets/story.jpg";
 const Story = () => {
   return (
     <div className="mt-20 w-full relative">
+      <div className="w-full h-full bg-white opacity-20 absolute"></div>
       <div className="absolute flex items-center w-full justify-center flex-col">
         <p className="text-white text-[50px] italic">Learn About us</p>
       </div>
