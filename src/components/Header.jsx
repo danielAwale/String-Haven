7c0f61c65f0c02a0fb07b7df2f570db894eb30f1
Daniel Awale <danielmadhukarawale@gmail.com>
2023-04-01 15:34:52 -0700

    work on the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,12 +3,16 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div className="flex justify-between py-4 px-4 items-center">
+    <div className="flex justify-between py-4 px-4 items-center w-full ml-3 mr-3">
       <div className="font-thin italic text-[30px] cursor-pointer">
-       <span className="font-bold">String</span> Haven
+        <span className="font-bold">String</span> Haven
       </div>
-      <div className="text-[35px] cursor-pointer">
-        <GiHamburgerMenu />
+      <div className="text-[25px] cursor-pointer flex justify-around items-center w-[50%]">
+        {/* <GiHamburgerMenu /> */}
+        <p>Acoustic</p>
+        <p>Electric</p>
+        <p>Bass</p>
+        <p>Ukulele</p>
       </div>
     </div>
   );
