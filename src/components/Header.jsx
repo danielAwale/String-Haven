4af3227e070ced217842f7da8ee5b15e1da83b96
Daniel Awale <danielmadhukarawale@gmail.com>
2023-04-01 15:51:33 -0700

    fix the navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 const Header = () => {
   return (
-    <div className="flex justify-between py-4 px-4 items-center w-full ml-3 mr-3">
-      <div className="font-thin italic text-[30px] cursor-pointer">
+    <div className="flex justify-around py-4 px-4 items-center max-w-full container">
+      <div className="font-thin italic text-[30px] cursor-pointer w-full">
         <span className="font-bold">String</span> Haven
       </div>
-      <div className="text-[25px] cursor-pointer flex justify-around items-center w-[50%]">
+      <div className="text-[22px] cursor-pointer flex justify-around items-center w-full">
         {/* <GiHamburgerMenu /> */}
         <p>Acoustic</p>
         <p>Electric</p>
