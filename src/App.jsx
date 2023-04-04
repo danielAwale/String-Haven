f993f366b030b57e589c26b7881aa80484e72574
Daniel Awale <danielmadhukarawale@gmail.com>
2023-04-04 11:57:53 -0700

    contain separate pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,16 @@
 import React from "react";
-import Header from "./components/Header";
-import Banner from "./components/Banner";
-import Policy from "./components/Policy";
-import Footer from "./components/Footer";
-import Carousel from "./components/Slider";
-import Story from "./components/Story";
+import Home from "./pages/Home";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <Header />
-      <Banner />
-      <div className=" mt-4">
-        <Policy />
-      </div>
-      <div className="flex justify-center w-full items-center mt-6">
-        <p className="text-xl font-bold">Shop Our Latest Collection</p>
-      </div>
-      <Carousel />
-      <div className="h-[800px]">
-        <Story />
-      </div>
-
-      <footer>
-        <Footer />
-      </footer>
+      <Router>
+        <Routes>
+          <Route />
+        </Routes>
+      </Router>
     </>
   );
 };
---
+++ src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import Policy from "../components/Policy";
+import Footer from "../components/Footer";
+import Carousel from "../components/Slider";
+import Story from "../components/Story";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Banner />
+      <div className=" mt-4">
+        <Policy />
+      </div>
+      <div className="flex justify-center w-full items-center mt-6">
+        <p className="text-xl font-bold">Shop Our Latest Collection</p>
+      </div>
+      <Carousel />
+      <div className="h-[800px]">
+        <Story />
+      </div>
+
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  );
+};
+
+export default Home;
