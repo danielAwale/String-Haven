0701af480785b1414d6489db8ace82b1e06ab5d4
Daniel Awale <danielmadhukarawale@gmail.com>
2023-04-11 15:40:46 -0700

    in process of getting data from firebase

--- src/pages/Acoustic.jsx
+++ src/pages/Acoustic.jsx
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { db } from "../config/firebase";
 
 import Img from "../assets/guitar2.jpeg";
+import { getDocs, collection } from "firebase/firestore";
 
 const Acoustic = () => {
+  const [guitar, setGuitar] = useState([]);
+
+  const guitarCollectionRef = collection(db, "acoustic");
+
+  const getGuitar = async () => {
+    try {
+      const data = await getDocs(guitarCollectionRef);
+    } catch (error) {}
+  };
+
   return (
     <section className="container m-auto grid">
       <div className="flex flex-col items-center border border-sky-600">
