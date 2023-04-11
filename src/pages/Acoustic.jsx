ee6da2006b5f156cfe44193a57819bd2d451966e
Daniel Awale <danielmadhukarawale@gmail.com>
2023-04-11 15:11:59 -0700

    work on styling the acoustic page

--- src/pages/Acoustic.jsx
+++ src/pages/Acoustic.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 
+import Img from "../assets/guitar2.jpeg";
+
 const Acoustic = () => {
   return (
-    <div>
-      <img src="" alt="" />
-      <p>StringMaster</p>
-      <p>2053</p>
-    </div>
+    <section className="flex">
+      <div>
+        <div>
+          <img src={Img} alt="" className="h-[800px]" />
+        </div>
+        <div>
+          <p>StringMaster</p>
+          <p>2053</p>
+        </div>
+      </div>
+    </section>
   );
 };
 
